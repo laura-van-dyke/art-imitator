9047fd2ec2904af2c8af51ab129b4c7b4edb26f5
Laura Van Dyke <laura.e.vandyke@gmail.com>
2021-06-18 18:23:09 -0400

    delete some unused

--- client/index.js
+++ client/index.js
@@ -1,16 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
-import App from './app'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import history from './history';
+import App from './app';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById('app')
-)
+);
